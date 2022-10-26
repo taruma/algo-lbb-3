0fa3956081e8727021e5dfafd0825101a7ece97b
taruma sakti <hi@taruma.info>
2022-10-26 17:03:51 +0700

    rubrik 1-2

--- app.R
+++ app.R
@@ -1,22 +1,15 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# -> RUBRIK LBB 3 <-
+# - (1 Point) Tahapan data pre-processing menggunakan dplyr 1 > filter/arange/mutate/groupby...
+# - (1 Point) Plot yang ditampilkan pada dashboard sudah interaktif > cukup pakai plotly atau ggplotly(...)
+# - (1 Point) Setiap plot yang ditampilkan menampilkan informasi yang relevan dari dashboard > informasinya apa?
 
 library(shiny)
-library(shinydashboard)
-library(dplyr)
 
-# algoscam
-
-ui <- shinydashboard::dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody(),
+ui <- shiny::fluidPage(
+  
+  # App title
+  
+  
 )
 
 # Define server logic required to draw a histogram
